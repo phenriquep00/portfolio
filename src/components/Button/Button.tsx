9fa0eaf25a9131f4740f4701953cf3015661709d
Pedro Lima <contact.pedrolima@gmail.com>
2022-08-10 18:17:16 -0300

    style work

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -16,11 +16,11 @@ export function Button({ children, type, onClick }: ButtonProps) {
                 :
                 type.startsWith("s")
                 ? // secondary button style
-                'flex items-center justify-center border-2 border-emerald-400 text-emerald-400 font-semibold p-2 rounded-md w-fit h-fit hover:cursor-pointer hover:border-emerald-200 hover:text-emerald-200 transition-colors duration-200'
+                'flex items-center justify-center border-2 border-emerald-400 text-emerald-400 font-bold p-[6px] rounded-md w-fit h-fit hover:cursor-pointer hover:border-emerald-200 hover:text-emerald-200 transition-colors duration-200'
                 :
                 type.startsWith("t")
                 ? // tertiary button style
-                'flex items-center justify-center text-emerald-400 font-semibold p-2 rounded-md w-fit h-fit hover:cursor-pointer hover:text-emerald-600 transition-colors duration-200'
+                'flex items-center justify-center text-emerald-400 font-semibold p-2 w-fit h-fit hover:cursor-pointer hover:text-emerald-600 transition-colors duration-200'
                 :
                 ''
             }
--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -4,11 +4,11 @@ export function Header() {
     return (
         <header className="flex flex-row justify-start items-center w-full h-[7%] bg-[#2A2338] text-white gap-6 pl-6">
             <p>phenriquep00</p>
-            <Button type="t" onClick={null}>github</Button> 
-            <Button type="t" onClick={null}>linkedin</Button> 
-            <Button type="p" onClick={null}>linkedin</Button> 
-            <Button type="s" onClick={null}>linkedin</Button> 
 
+
+            <Button type="p" onClick={null}>primary</Button>
+            <Button type="s" onClick={null}>secondary</Button>
+            <Button type="t" onClick={null}>tertiary</Button>
         </header>
     )
 }
