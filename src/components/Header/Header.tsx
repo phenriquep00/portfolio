831ab52fbf2e7621440b12a19c6c021c6682c640
Pedro Lima <contact.pedrolima@gmail.com>
2022-08-10 17:51:12 -0300

    started header structure

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,11 @@
+export function Header() {
+    return (
+        <header className="flex flex-row justify-start items-center w-full h-[7%] bg-black text-white gap-6 pl-6">
+            <p>phenriquep00</p>
+            <p>github</p>
+            <p>linkedin</p>
+            <p>tweeter</p>
+
+        </header>
+    )
+}
--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -1,13 +1,10 @@
 import { Button } from "../../components/Button/Button";
+import { Header } from "../../components/Header/Header";
 
 export function HomePage() {
     return (
-        <div className="flex flex-column w-screen h-screen items-center justify-center">
-            <div className="gap-4 flex flex-row">
-                <Button type="p" onClick={null}>primary</Button>
-                <Button type="s" onClick={null}>secondary</Button>
-                <Button type="t" onClick={null}>tertiary</Button>
-            </div>
+        <div className="flex flex-column w-screen h-screen">
+            <Header />
 
         </div>
     )
