51e5a40e2bb159b3cb03894e1dec04317f3cda34
Pedro Lima <contact.pedrolima@gmail.com>
2022-08-10 17:39:35 -0300

    seconrary button stylized

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -12,11 +12,11 @@ export function Button({ children, type, onClick }: ButtonProps) {
             className={
                 type.startsWith("p")
                 ? // primary button style
-                'flex items-center justify-center bg-emerald-700 text-white font-semibold p-2 rounded-md w-fit h-fit hover:cursor-pointer hover:bg-emerald-600 hover:ring-2 ring-offset-1'
+                'flex items-center justify-center bg-emerald-700 text-white font-semibold p-2 rounded-md w-fit h-fit hover:cursor-pointer hover:bg-emerald-600 hover:ring-2 ring-offset-1 transition-colors duration-200'
                 :
                 type.startsWith("s")
                 ? // secondary button style
-                ''
+                'flex items-center justify-center border-2 border-emerald-700 text-emerald-700 font-semibold p-2 rounded-md w-fit h-fit hover:cursor-pointer hover:border-emerald-500 hover:text-emerald-500 transition-colors duration-200'
                 :
                 type.startsWith("t")
                 ? // tertiary button style
--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -3,7 +3,12 @@ import { Button } from "../../components/Button/Button";
 export function HomePage() {
     return (
         <div className="flex flex-column w-screen h-screen items-center justify-center">
-            <Button type="p" onClick={null}>Test</Button>
-        </div>   
+            <div className="gap-4 flex flex-row">
+                <Button type="p" onClick={null}>primary</Button>
+                <Button type="s" onClick={null}>secondary</Button>
+                <Button type="t" onClick={null}>tertiary</Button>
+            </div>
+
+        </div>
     )
 }
