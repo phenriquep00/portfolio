9c2fa873ffbffe8555cbb32326ae901fd61dff9a
Pedro Lima <contact.pedrolima@gmail.com>
2022-08-11 16:31:01 -0300

    resized logos

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,6 +1,7 @@
 import { FacebookLogo, GithubLogo, InstagramLogo, LinkedinLogo, TwitterLogo } from "phosphor-react";
 import { useEffect, useState } from "react";
 import { Button } from "../Button/Button";
+import { Logo } from "../Logo/Logo";
 
 export function Header() {
 
@@ -17,9 +18,19 @@ export function Header() {
 
     return (
         <header className="flex flex-row justify-start items-center w-full h-[7%] bg-[#2A2338] text-white gap-4 pl-6">
-            <p>phenriquep00</p>
+
+            <Logo img="https://github.com/phenriquep00.png">
+            {
+                    windowDimensions.width > 600 && windowDimensions.height > 800
+                        ?
+                        <p>phenriquep00</p>
+                        :
+                        null
+                }
+            </Logo>
+
             <Button type="t" onClick={null}>
-                <GithubLogo weight="fill" size={18} className="mr-1" />
+                <GithubLogo weight="fill" size={20} className="mr-1" />
                 {
                     windowDimensions.width > 600 && windowDimensions.height > 800
                         ?
@@ -31,7 +42,7 @@ export function Header() {
             </Button>
 
             <Button type="t" onClick={null}>
-                <LinkedinLogo weight="fill" size={18} className="mr-1" />
+                <LinkedinLogo weight="fill" size={20} className="mr-1" />
                 {
                     windowDimensions.width > 600 && windowDimensions.height > 800
                         ?
@@ -42,7 +53,7 @@ export function Header() {
             </Button>
 
             <Button type="t" onClick={null}>
-                <FacebookLogo weight="fill" size={18} className="mr-1" />
+                <FacebookLogo weight="fill" size={20} className="mr-1" />
                 {
                     windowDimensions.width > 600 && windowDimensions.height > 800
                         ?
@@ -53,7 +64,7 @@ export function Header() {
             </Button>
 
             <Button type="t" onClick={null}>
-                <InstagramLogo weight="fill" size={18} className="mr-1" />
+                <InstagramLogo weight="fill" size={20} className="mr-1" />
                 {
                     windowDimensions.width > 600 && windowDimensions.height > 800
                         ?
@@ -64,7 +75,7 @@ export function Header() {
             </Button>
 
             <Button type="t" onClick={null}>
-                <TwitterLogo weight="fill" size={18} className="mr-1" />
+                <TwitterLogo weight="fill" size={20} className="mr-1" />
                 {
                     windowDimensions.width > 600 && windowDimensions.height > 800
                         ?
--- src/components/Logo/Logo.tsx
+++ src/components/Logo/Logo.tsx
@@ -1,11 +1,14 @@
-type LogoProps = {
-    
-}
+import { PropsWithChildren } from "react";
 
-export function Logo() {
+type LogoProps = PropsWithChildren<{
+    img: string;
+}>
+
+export function Logo({ img, children }: LogoProps) {
     return (
-        <div>
-
+        <div className="flex flex-row items-center justify-center gap-2">
+            <img src={img} alt="profile image" className="w-8 rounded-full mt-2"/>
+            <p className="font-semibold text-base">{children}</p>
         </div>
     )
 }
