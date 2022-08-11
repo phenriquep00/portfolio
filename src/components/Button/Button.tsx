c730d9e88dcfeb6e1705e422f905e48238693545
Pedro Lima <contact.pedrolima@gmail.com>
2022-08-11 16:18:59 -0300

    added social midea buttons to header

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -8,21 +8,21 @@ type ButtonProps = PropsWithChildren<{
 
 export function Button({ children, type, onClick }: ButtonProps) {
     return (
-        <div 
+        <div
             className={
                 type.startsWith("p")
-                ? // primary button style
-                'flex items-center justify-center bg-emerald-500 text-white font-semibold p-2 rounded-md w-fit h-fit hover:cursor-pointer hover:bg-emerald-600 hover:ring-2 ring-offset-1 transition-colors duration-200'
-                :
-                type.startsWith("s")
-                ? // secondary button style
-                'flex items-center justify-center border-2 border-emerald-400 text-emerald-400 font-bold p-[6px] rounded-md w-fit h-fit hover:cursor-pointer hover:border-emerald-200 hover:text-emerald-200 transition-colors duration-200'
-                :
-                type.startsWith("t")
-                ? // tertiary button style
-                'flex items-center justify-center text-emerald-400 font-semibold p-2 w-fit h-fit hover:cursor-pointer hover:text-emerald-600 transition-colors duration-200'
-                :
-                ''
+                    ? // primary button style
+                    'flex flex-row items-center justify-center bg-emerald-500 text-white font-semibold p-2 rounded-md w-fit h-fit hover:cursor-pointer hover:bg-emerald-600 hover:ring-2 ring-offset-1 transition-colors duration-200'
+                    :
+                    type.startsWith("s")
+                        ? // secondary button style
+                        'flex flex-row items-center justify-center border-2 border-emerald-400 text-emerald-400 font-bold p-[6px] rounded-md w-fit h-fit hover:cursor-pointer hover:border-emerald-200 hover:text-emerald-200 transition-colors duration-200'
+                        :
+                        type.startsWith("t")
+                            ? // tertiary button style
+                            'flex flex-row items-center justify-center text-emerald-400 font-semibold p-2 w-fit h-fit hover:cursor-pointer hover:text-emerald-600 transition-colors duration-200'
+                            :
+                            ''
             }
         >
             {children}
--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,14 +1,86 @@
+import { FacebookLogo, GithubLogo, InstagramLogo, LinkedinLogo, TwitterLogo } from "phosphor-react";
+import { useEffect, useState } from "react";
 import { Button } from "../Button/Button";
 
 export function Header() {
+
+    const [windowDimensions, setWindowDimensions] = useState(getWindowDimensions());
+
+    useEffect(() => {
+        function handleResize() {
+            setWindowDimensions(getWindowDimensions());
+        }
+
+        window.addEventListener('resize', handleResize);
+        return () => window.removeEventListener('resize', handleResize);
+    }, []);
+
     return (
-        <header className="flex flex-row justify-start items-center w-full h-[7%] bg-[#2A2338] text-white gap-6 pl-6">
+        <header className="flex flex-row justify-start items-center w-full h-[7%] bg-[#2A2338] text-white gap-4 pl-6">
             <p>phenriquep00</p>
+            <Button type="t" onClick={null}>
+                <GithubLogo weight="fill" size={18} className="mr-1" />
+                {
+                    windowDimensions.width > 600 && windowDimensions.height > 800
+                        ?
+                        <p>Github</p>
+                        :
+                        null
+                }
 
+            </Button>
 
-            <Button type="p" onClick={null}>primary</Button>
-            <Button type="s" onClick={null}>secondary</Button>
-            <Button type="t" onClick={null}>tertiary</Button>
+            <Button type="t" onClick={null}>
+                <LinkedinLogo weight="fill" size={18} className="mr-1" />
+                {
+                    windowDimensions.width > 600 && windowDimensions.height > 800
+                        ?
+                        <p>Linkedin</p>
+                        :
+                        null
+                }
+            </Button>
+
+            <Button type="t" onClick={null}>
+                <FacebookLogo weight="fill" size={18} className="mr-1" />
+                {
+                    windowDimensions.width > 600 && windowDimensions.height > 800
+                        ?
+                        <p>Facebook</p>
+                        :
+                        null
+                }
+            </Button>
+
+            <Button type="t" onClick={null}>
+                <InstagramLogo weight="fill" size={18} className="mr-1" />
+                {
+                    windowDimensions.width > 600 && windowDimensions.height > 800
+                        ?
+                        <p>Instagram</p>
+                        :
+                        null
+                }
+            </Button>
+
+            <Button type="t" onClick={null}>
+                <TwitterLogo weight="fill" size={18} className="mr-1" />
+                {
+                    windowDimensions.width > 600 && windowDimensions.height > 800
+                        ?
+                        <p>Twitter</p>
+                        :
+                        null
+                }
+            </Button>
         </header>
     )
+}
+
+function getWindowDimensions() {
+    const { innerWidth: width, innerHeight: height } = window;
+    return {
+        width,
+        height
+    };
 }
