2a4bfa15baf4e71f1f91bcd68123f1ba392b20a7
Pedro Lima <contact.pedrolima@gmail.com>
2022-08-15 16:56:06 -0300

    created content components, about me, projects and contacts

---
+++ src/components/Content/Contacts/Contacts.tsx
@@ -0,0 +1,7 @@
+export function Contacts() {
+    return(
+        <div>
+            Contacts
+        </div>
+    )
+}
---
+++ src/components/Content/Projects/Projects.tsx
@@ -0,0 +1,7 @@
+export function Projects() {
+    return (
+        <div>
+            Projects
+        </div>
+    )
+}
