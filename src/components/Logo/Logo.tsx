81ff0ee4dc84e827cb79f7b8e3bc699156dfbee0
Pedro Lima <contact.pedrolima@gmail.com>
2022-08-15 17:22:41 -0300

    color scheeme update

--- src/components/Logo/Logo.tsx
+++ src/components/Logo/Logo.tsx
@@ -6,7 +6,7 @@ type LogoProps = PropsWithChildren<{
 
 export function Logo({ img, children }: LogoProps) {
     return (
-        <div className="flex flex-row items-center justify-center gap-2">
+        <div className="flex flex-row items-center justify-center gap-2 cursor-default">
             <img src={img} alt="profile image" className="w-8 rounded-full justify-self-center"/>
             <p className="font-semibold text-base">{children}</p>
         </div>
