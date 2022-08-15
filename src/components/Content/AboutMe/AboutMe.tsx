4b397b1aeeaa19a41ce015949e234f82bad38835
Pedro Lima <contact.pedrolima@gmail.com>
2022-08-15 17:26:19 -0300

    working on about me links

--- src/components/Content/AboutMe/AboutMe.tsx
+++ src/components/Content/AboutMe/AboutMe.tsx
@@ -2,9 +2,9 @@ import { Button } from "../../Button/Button";
 
 export function AboutMe() {
     return (
-        <div>
+        <div className="text-white">
             About Me
-            <div>
+            <div className="flex flex-row gap-3">
                 <Button type={"p"} onClick={null}> Check Projects</Button>
                 <Button type={"s"} onClick={null}> contacts</Button>
             </div>
