72d52ea65ec13d60a8fb2e1859767e76cfe863e3
Pedro Lima <contact.pedrolima@gmail.com>
2022-08-16 16:30:28 -0300

    added todo to about me section

--- src/components/Content/AboutMe/AboutMe.tsx
+++ src/components/Content/AboutMe/AboutMe.tsx
@@ -9,12 +9,20 @@ export function AboutMe({ updateActiveContent }: AboutMeProps) {
     const updateCurrent = (
         e: React.MouseEvent<HTMLInputElement>
     ) => {
-       updateActiveContent(e.currentTarget.innerText)
+        updateActiveContent(e.currentTarget.innerText)
     }
 
     return (
         <div className="text-white">
+
             About Me
+
+            {/* 
+                what do I want in the about me section?
+                - greeting message
+                - working technologies
+                - something interesting
+            */}
             <div className="flex flex-row gap-3">
                 <Button type={"p"} onClick={updateCurrent}>Projects</Button>
                 <Button type={"s"} onClick={updateCurrent}>Contacts</Button>
