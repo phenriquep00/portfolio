5d1c2b917cd4933de1f14b0e43483b43e0b80346
Pedro Lima <contact.pedrolima@gmail.com>
2022-08-16 16:33:33 -0300

    added contacts todo

--- src/components/Content/Contacts/Contacts.tsx
+++ src/components/Content/Contacts/Contacts.tsx
@@ -2,6 +2,14 @@ export function Contacts() {
     return(
         <div>
             Contacts
+
+            {/* 
+                what do I want in the contacts section?
+                social media display
+                email section to send mails to me
+
+            */}
+            
         </div>
     )
 }
