0864ec9612d243493b04e01a27ee7440e4d46e98
Pedro Lima <contact.pedrolima@gmail.com>
2022-08-22 16:15:21 -0300

    added guide to carrousel

--- src/components/Carrousel/Carrousel.tsx
+++ src/components/Carrousel/Carrousel.tsx
@@ -2,6 +2,8 @@ import { CarrouselDescription } from "./CarrouselDescription";
 import { CarrouselImage } from "./CarrouselImage";
 import { CarrouselSlider } from "./CarrouselSlider";
 
+// TODO: Change the color of the background based on the main color of the image
+
 export function Carrousel() {
     return (
         <div className="text-white w-full h-full flex flex-col items-center justify-center">
