821d7233321c8bf497f629df1b532d6a416b6283
Pedro Lima <contact.pedrolima@gmail.com>
2022-08-23 17:22:58 -0300

    added card component to slider

--- src/components/Carrousel/CarrouselSlider.tsx
+++ src/components/Carrousel/CarrouselSlider.tsx
@@ -1,7 +1,14 @@
+import { CarrouselSliderCard } from "./CarrouselSliderCard";
+
 export function CarrouselSlider() {
     return (
-        <div className="bg-white bg-opacity-40">
-            Carrousel Slider
+        <div className="bg-white bg-opacity-40 absolute md:bottom-6 md:left-12 bottom-4 right-2 md:w-[70%] w-[85%] h-32 rounded-md flex flex-row items-center p-2 gap-2">
+            <CarrouselSliderCard />
+            <CarrouselSliderCard />
+            <CarrouselSliderCard />
+            <CarrouselSliderCard />
+            <CarrouselSliderCard />
+            <CarrouselSliderCard />
         </div>
     )
 }
---
+++ src/components/Carrousel/CarrouselSliderCard.tsx
@@ -0,0 +1,7 @@
+export function CarrouselSliderCard() {
+    return (
+        <div className="bg-black w-40 h-28 flex items-center justify-center rounded-md">
+            Card
+        </div>
+    )
+}
