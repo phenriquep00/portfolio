91633c8088b73d82253bc6693d06710be3e019f7
Pedro Lima <contact.pedrolima@gmail.com>
2022-08-23 17:35:15 -0300

    connected card component to data file

--- src/components/Carrousel/CarrouselSlider.tsx
+++ src/components/Carrousel/CarrouselSlider.tsx
@@ -1,14 +1,16 @@
+import { CaretRight } from "phosphor-react";
 import { CarrouselSliderCard } from "./CarrouselSliderCard";
+import { projectsData, ProjectsProps } from "../../data";
 
 export function CarrouselSlider() {
     return (
-        <div className="bg-white bg-opacity-40 absolute md:bottom-6 md:left-12 bottom-4 right-2 md:w-[70%] w-[85%] h-32 rounded-md flex flex-row items-center p-2 gap-2">
-            <CarrouselSliderCard />
-            <CarrouselSliderCard />
-            <CarrouselSliderCard />
-            <CarrouselSliderCard />
-            <CarrouselSliderCard />
-            <CarrouselSliderCard />
+        <div className="bg-white bg-opacity-30 absolute md:bottom-6 md:left-12 bottom-4 right-2 md:w-[70%] w-[85%] h-32 rounded-md flex flex-row items-center p-2 gap-2">
+            {
+                projectsData.map(({ id, name, thumb }: ProjectsProps) => (
+                    <CarrouselSliderCard key={id} />
+                ))
+            }
+            <CaretRight weight="bold" size={26} className="absolute right-1 cursor-pointer" />
         </div>
     )
 }
--- src/data.tsx
+++ src/data.tsx
@@ -25,13 +25,29 @@ export interface ProjectsProps {
     name: string;
     img: string;
     description: string;
+    thumb: string;
 }
 
-export const ProjectsData: ProjectsProps[] = [
+export const projectsData: ProjectsProps[] = [
     {
         id: 0,
         name: 'test1',
         img: 'test1.png',
         description: 'test1 description',
-    }
+        thumb: 'thumb.png'
+    },
+    {
+        id: 1,
+        name: 'test2',
+        img: 'test2.png',
+        description: 'test2 description',
+        thumb: 'thumb.png'
+    },
+    {
+        id: 2,
+        name: 'test3',
+        img: 'test3.png',
+        description: 'test3 description',
+        thumb: 'thumb.png'
+    },
 ]
