315d2c8c9d93dee5d6c3f982a8fe7c7edc48d214
Pedro Lima <contact.pedrolima@gmail.com>
2022-08-24 18:35:51 -0300

    finalized carousel motion

--- src/components/Carrousel/CarrouselSlider.tsx
+++ src/components/Carrousel/CarrouselSlider.tsx
@@ -2,39 +2,30 @@ import { CaretRight } from "phosphor-react";
 import { CarrouselSliderCard } from "./CarrouselSliderCard";
 import { projectsData, ProjectsProps } from "../../data";
 import { useEffect, useRef, useState } from "react";
+import { motion } from 'framer-motion';
 
 
 export function CarrouselSlider() {
-    
-    const [activeProjects, setActiveProjects] = useState([0, 1, 2, 3, 4, 5])
-    const divRef = useRef<HTMLDivElement>(null)
 
-    const handleArrowRight = () => {
-        activeProjects.map((element, index) => {
-            const arr = activeProjects
-            arr[index] = element + 1
-            setActiveProjects(arr)
-        })
-    }
+    const [ width, setWidth ] = useState(0);
+    const carrousel = useRef<HTMLDivElement>(null);
 
     useEffect(() => {
-        divRef.current?.focus();
-        console.log('updated')
-    }, [activeProjects])
+        if (carrousel.current?.scrollWidth != undefined && carrousel.current?.offsetWidth != undefined) {
+            setWidth(carrousel.current?.scrollWidth - carrousel.current?.offsetWidth)
+        }
+    }, [])
 
     return (
-        <div  className="bg-white bg-opacity-30 absolute md:bottom-6 md:left-12 bottom-4 right-6 md:w-[70%] w-[85%] h-32 rounded-md flex flex-row flex-grow-0 items-center p-2 gap-2 overflow-hidden">
-            {
-                projectsData.map(({ id, name, thumb }: ProjectsProps) => (
-                    activeProjects.includes(id) ?
-                    <div ref={divRef}>
-                       <CarrouselSliderCard key={id} text={name}/> 
-                    </div>
-                    : null
-                ))
-            }
-            <CaretRight weight="bold" size={26} className="absolute right-0 cursor-pointer bg-black bg-opacity-60 h-28 opacity-80 md:hover:opacity-100 " onClick={handleArrowRight}/>
-        </div>
+        <motion.div ref={carrousel} className="bg-white bg-opacity-30 absolute md:bottom-6 md:left-12 bottom-4 right-6 md:w-[75%] w-[85%] h-32 rounded-md flex flex-row flex-grow-0 items-center overflow-hidden">
+            <motion.div drag='x' dragConstraints={{right: 0, left: -width}} className="flex gap-3 px-2">
+                {
+                    projectsData.map(({ id, name, thumb }: ProjectsProps) => (
+                        <CarrouselSliderCard key={id} text={name} />
+                    ))
+                }
+            </motion.div>
+        </motion.div>
     )
-    
+
 }
