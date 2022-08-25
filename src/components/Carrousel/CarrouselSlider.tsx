59dd04473c2936d8060a5753ca005894e8611274
Pedro Lima <contact.pedrolima@gmail.com>
2022-08-25 17:10:53 -0300

    slider card interactivity update

--- src/components/Carrousel/CarrouselSlider.tsx
+++ src/components/Carrousel/CarrouselSlider.tsx
@@ -1,4 +1,3 @@
-import { CaretRight } from "phosphor-react";
 import { CarrouselSliderCard } from "./CarrouselSliderCard";
 import { projectsData, ProjectsProps } from "../../data";
 import { useEffect, useRef, useState } from "react";
@@ -7,7 +6,9 @@ import { motion } from 'framer-motion';
 
 export function CarrouselSlider() {
 
-    const [ width, setWidth ] = useState(0);
+    const [width, setWidth] = useState(0);
+    const [activeId, setActiveId] = useState(0)
+
     const carrousel = useRef<HTMLDivElement>(null);
 
     useEffect(() => {
@@ -18,10 +19,15 @@ export function CarrouselSlider() {
 
     return (
         <motion.div ref={carrousel} className="bg-white bg-opacity-30 absolute md:bottom-6 md:left-12 bottom-4 right-6 md:w-[75%] w-[85%] h-32 rounded-md flex flex-row flex-grow-0 items-center overflow-hidden">
-            <motion.div drag='x' dragConstraints={{right: 0, left: -width}} className="flex gap-3 px-2">
+            <motion.div drag='x' dragConstraints={{ right: 0, left: -width }} className="flex gap-3 px-2">
                 {
                     projectsData.map(({ id, name, thumb }: ProjectsProps) => (
-                        <CarrouselSliderCard key={id} text={name} />
+
+                        id === activeId
+                            ?
+                            <CarrouselSliderCard key={id} text={name} active={true} />
+                            :
+                            <CarrouselSliderCard key={id} text={name} active={false} />
                     ))
                 }
             </motion.div>
--- src/components/Carrousel/CarrouselSliderCard.tsx
+++ src/components/Carrousel/CarrouselSliderCard.tsx
@@ -1,11 +1,23 @@
+import { useEffect, useRef, useState } from "react";
+
 interface CarrouselSliderCardProps {
     text: string;
+    active: boolean;
 }
 
-export function CarrouselSliderCard({ text }: CarrouselSliderCardProps) {
-    // TODO: add ring when focused (useref?)
+export function CarrouselSliderCard({ text, active }: CarrouselSliderCardProps) {
+
+    const [isActive, setIsActive] = useState(active)
+
     return (
-        <div className="bg-gray-700 w-40 h-28 flex items-center justify-center rounded-md flex-shrink-0 cursor-pointer hover:ring-4 ring-offset-1">
+        <div
+            className={
+                `
+                bg-gray-700 w-40 h-28 flex items-center justify-center rounded-md flex-shrink-0 cursor-pointer 
+                ${isActive ? 'ring-2 ring-orange-400' : 'hover:ring-4'} ring-offset-1
+                `
+            }
+        >
             {text}
         </div>
     )
