a5f9661a633151edc964f9b0118825b6b2437632
Pedro Lima <contact.pedrolima@gmail.com>
2022-08-25 17:59:54 -0300

    carousel slider update

--- src/components/Carrousel/CarrouselSlider.tsx
+++ src/components/Carrousel/CarrouselSlider.tsx
@@ -1,15 +1,16 @@
 import { CarrouselSliderCard } from "./CarrouselSliderCard";
 import { projectsData, ProjectsProps } from "../../data";
-import { useEffect, useRef, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import { motion } from 'framer-motion';
 
 
 export function CarrouselSlider() {
 
     const [width, setWidth] = useState(0);
-    const [activeId, setActiveId] = useState(0)
+
 
     const carrousel = useRef<HTMLDivElement>(null);
+    
 
     useEffect(() => {
         if (carrousel.current?.scrollWidth != undefined && carrousel.current?.offsetWidth != undefined) {
@@ -22,12 +23,7 @@ export function CarrouselSlider() {
             <motion.div drag='x' dragConstraints={{ right: 0, left: -width }} className="flex gap-3 px-2">
                 {
                     projectsData.map(({ id, name, thumb }: ProjectsProps) => (
-
-                        id === activeId
-                            ?
-                            <CarrouselSliderCard key={id} text={name} active={true} />
-                            :
-                            <CarrouselSliderCard key={id} text={name} active={false} />
+                            <CarrouselSliderCard key={id} text={name} />
                     ))
                 }
             </motion.div>
--- src/components/Carrousel/CarrouselSliderCard.tsx
+++ src/components/Carrousel/CarrouselSliderCard.tsx
@@ -1,22 +1,16 @@
-import { useEffect, useRef, useState } from "react";
+
 
 interface CarrouselSliderCardProps {
     text: string;
-    active: boolean;
 }
 
-export function CarrouselSliderCard({ text, active }: CarrouselSliderCardProps) {
+export function CarrouselSliderCard({ text }: CarrouselSliderCardProps) {
 
-    const [isActive, setIsActive] = useState(active)
+
 
     return (
         <div
-            className={
-                `
-                bg-gray-700 w-40 h-28 flex items-center justify-center rounded-md flex-shrink-0 cursor-pointer 
-                ${isActive ? 'ring-2 ring-orange-400' : 'hover:ring-4'} ring-offset-1
-                `
-            }
+            className="bg-gray-700 w-40 h-28 flex items-center justify-center rounded-md flex-shrink-0 cursor-pointer hover:ring-4 ring-offset-1"
         >
             {text}
         </div>
