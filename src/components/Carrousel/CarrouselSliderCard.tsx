91b0ad2d618440942a176f069127164b795646ed
Pedro Lima <contact.pedrolima@gmail.com>
2022-08-26 18:12:11 -0300

    fixing animation scales

--- src/components/Carrousel/CarrouselSliderCard.tsx
+++ src/components/Carrousel/CarrouselSliderCard.tsx
@@ -10,7 +10,7 @@ export function CarrouselSliderCard({ text, onClick }: CarrouselSliderCardProps)
 
     return (
         <motion.div
-            whileHover={{scale:1.1}}
+            whileHover={{scale:1.05 }}
             onClick={onClick}
             className="bg-gray-700 w-40 h-28 flex items-center justify-center rounded-md flex-shrink-0 cursor-pointer ring-violet-600 hover:ring-4 ring-offset-1"
         >
