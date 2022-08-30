cb22a688aecf0b1cc591ec18909f6fedec1c2b5b
Pedro Lima <contact.pedrolima@gmail.com>
2022-08-30 14:40:07 -0300

    start working with contacts content component

--- src/components/Content/Contacts/Contacts.tsx
+++ src/components/Content/Contacts/Contacts.tsx
@@ -1,15 +1,13 @@
+
+
+
 export function Contacts() {
-    return(
+
+    return (
         <div>
-            Contacts
 
-            {/* 
-                what do I want in the contacts section?
-                social media display
-                email section to send mails to me
+        contacts
 
-            */}
-            
         </div>
     )
 }
