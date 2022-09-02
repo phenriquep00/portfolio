5bbb8c32891d781390a2a8607c9a36a979ab2c55
Pedro Lima <contact.pedrolima@gmail.com>
2022-09-02 17:02:34 -0300

    formated contacts and added links to every media

--- src/components/Content/Contacts/Contacts.tsx
+++ src/components/Content/Contacts/Contacts.tsx
@@ -10,28 +10,40 @@ export function Contacts() {
 
             <div className="flex flex-row gap-4 font-medium">
                 <Card title={<p className="flex flex-row gap-2"><GithubLogo size={20} /> Github</p>}>
-                    Teste
+                    <p className="text-cyan-600">
+                        <a href="https://github.com/phenriquep00" target={"_blank"}>phenriquep00</a>
+                    </p>
                 </Card>
-                <Card title={<p className="flex flex-row gap-2"><LinkedinLogo size={20}/> Linkedin</p>}>
-                    Teste2
-                </Card>
-            </div>
-
-            <div className="flex flex-row gap-4 font-medium">
-                <Card title={<p className="flex flex-row gap-2"><FacebookLogo size={20}/> Facebook</p>}>
-                    Teste
-                </Card>
-                <Card title={<p className="flex flex-row gap-2"><InstagramLogo size={20}/> Instagram</p>}>
-                    Teste2
+                <Card title={<p className="flex flex-row gap-2"><LinkedinLogo size={20} /> Linkedin</p>}>
+                    <p className="text-cyan-600">
+                        <a href="https://www.linkedin.com/in/pedro-lima-255a33223/" target={"_blank"}>Pedro Henrique</a>
+                    </p>
                 </Card>
             </div>
 
             <div className="flex flex-row gap-4 font-medium">
-                <Card title={<p className="flex flex-row gap-2"><TwitterLogo size={20}/> Twitter</p>}>
-                    Teste
+                <Card title={<p className="flex flex-row gap-2"><FacebookLogo size={20} /> Facebook</p>}>
+                    <p className="text-cyan-600">
+                        <a href="https://www.facebook.com/pedro.henriquefonseca.925/" target={"_blank"}>Add me on Facebook</a>
+                    </p>
                 </Card>
-                <Card title={<p className="flex flex-row gap-2"><WhatsappLogo size={20}/> Whatsapp</p>}>
-                    Teste2
+                <Card title={<p className="flex flex-row gap-2"><InstagramLogo size={20} /> Instagram</p>}>
+                    <p className="text-cyan-600">
+                        <a href="https://www.instagram.com/im.pedrooo/" target={"_blank"}>Follow on Instagram</a>
+                    </p>
+                </Card>
+            </div>
+
+            <div className="flex flex-row gap-4 font-medium">
+                <Card title={<p className="flex flex-row gap-2"><TwitterLogo size={20} /> Twitter</p>}>
+                    <p className="text-cyan-600">
+                        <a href="https://twitter.com/Pedro32977686" target={"_blank"}>Follow on Twitter</a>
+                    </p>
+                </Card>
+                <Card title={<p className="flex flex-row gap-2"><WhatsappLogo size={20} /> Whatsapp</p>}>
+                    <p className="text-cyan-600">
+                        <a href="https://wa.me/5599981277776" target={"_blank"}>Send a Message in Whatsapp</a>
+                    </p>
                 </Card>
             </div>
 
