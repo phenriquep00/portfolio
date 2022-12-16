ca64e87427656def4a5b4e4d9bc7a23cc06b276c
Pedro Lima <contact.pedrolima@gmail.com>
2022-12-16 14:08:26 -0300

    updated projects

--- data.ts
+++ data.ts
@@ -8,43 +8,75 @@ export interface IData {
 
 export const data: IData[] = [
   {
-    title: "First project",
+    title: "TODOApp",
     img: "",
     description:
       "Non tempor velit aute sunt labore eu deserunt eiusmod anim ex ad. Esse proident est esse pariatur irure. Do consectetur tempor officia aliqua fugiat officia excepteur labore adipisicing tempor est adipisicing nulla. Eiusmod eu est veniam incididunt fugiat labore amet elit. Labore duis dolore veniam veniam veniam exercitation dolor ullamco. Aliquip elit veniam ut ea aute do proident esse cillum. Amet reprehenderit laboris enim tempor labore quis esse magna cupidatat nulla nisi Lorem do in.",
     status: "active",
-    technologies: "js, html, css",
+    technologies: "typescript, react, vite, tailwindcss, supabase, ...",
   },
   {
-    title: "Second project",
+    title: "Poker",
     img: "",
     description:
       "Fugiat ut aute nostrud culpa fugiat sunt quis laboris incididunt veniam id commodo in ullamco. Sit officia culpa esse tempor non minim ea aliqua. Sit minim aliqua ex non Lorem aute dolore. Consectetur qui reprehenderit do aute enim est sunt labore occaecat adipisicing ea eiusmod. Reprehenderit laborum sint ullamco voluptate aliqua nisi in excepteur Lorem voluptate irure enim.",
     status: "in development",
-    technologies: "python, react",
+    technologies: "python, pygame",
   },
   {
-    title: "Third project",
+    title: "Beat Machine",
     img: "",
     description:
       "Fugiat ut aute nostrud culpa fugiat sunt quis laboris incididunt veniam id commodo in ullamco. Sit officia culpa esse tempor non minim ea aliqua. Sit minim aliqua ex non Lorem aute dolore. Consectetur qui reprehenderit do aute enim est sunt labore occaecat adipisicing ea eiusmod. Reprehenderit laborum sint ullamco voluptate aliqua nisi in excepteur Lorem voluptate irure enim.",
     status: "offline",
-    technologies: "nodejs, react native",
+    technologies: "python, pygame",
   },
   {
-    title: "Forth project",
+    title: "Nlw e-sports",
     img: "",
     description:
       "Fugiat ut aute nostrud culpa fugiat sunt quis laboris incididunt veniam id commodo in ullamco. Sit officia culpa esse tempor non minim ea aliqua. Sit minim aliqua ex non Lorem aute dolore. Consectetur qui reprehenderit do aute enim est sunt labore occaecat adipisicing ea eiusmod. Reprehenderit laborum sint ullamco voluptate aliqua nisi in excepteur Lorem voluptate irure enim.",
     status: "in development",
-    technologies: "python, react",
+    technologies: "typescript, react, react-native, nodejs, tailwindcss",
   },
   {
-    title: "Fifth project",
+    title: "Page clones",
     img: "",
     description:
       "Fugiat ut aute nostrud culpa fugiat sunt quis laboris incididunt veniam id commodo in ullamco. Sit officia culpa esse tempor non minim ea aliqua. Sit minim aliqua ex non Lorem aute dolore. Consectetur qui reprehenderit do aute enim est sunt labore occaecat adipisicing ea eiusmod. Reprehenderit laborum sint ullamco voluptate aliqua nisi in excepteur Lorem voluptate irure enim.",
     status: "in development",
-    technologies: "python, react",
+    technologies: "typescript, reacj, tailwindcss",
+  },
+  {
+    title: "Imersão Dados",
+    img: "",
+    description:
+      "Fugiat ut aute nostrud culpa fugiat sunt quis laboris incididunt veniam id commodo in ullamco. Sit officia culpa esse tempor non minim ea aliqua. Sit minim aliqua ex non Lorem aute dolore. Consectetur qui reprehenderit do aute enim est sunt labore occaecat adipisicing ea eiusmod. Reprehenderit laborum sint ullamco voluptate aliqua nisi in excepteur Lorem voluptate irure enim.",
+    status: "in development",
+    technologies: "Python, Pandas",
+  },
+  {
+    title: "Nlw Return",
+    img: "",
+    description:
+      "Fugiat ut aute nostrud culpa fugiat sunt quis laboris incididunt veniam id commodo in ullamco. Sit officia culpa esse tempor non minim ea aliqua. Sit minim aliqua ex non Lorem aute dolore. Consectetur qui reprehenderit do aute enim est sunt labore occaecat adipisicing ea eiusmod. Reprehenderit laborum sint ullamco voluptate aliqua nisi in excepteur Lorem voluptate irure enim.",
+    status: "in development",
+    technologies: "typescript, react, tailwindcss, nodejs, react-native",
+  },
+  {
+    title: "Pong",
+    img: "",
+    description:
+      "Fugiat ut aute nostrud culpa fugiat sunt quis laboris incididunt veniam id commodo in ullamco. Sit officia culpa esse tempor non minim ea aliqua. Sit minim aliqua ex non Lorem aute dolore. Consectetur qui reprehenderit do aute enim est sunt labore occaecat adipisicing ea eiusmod. Reprehenderit laborum sint ullamco voluptate aliqua nisi in excepteur Lorem voluptate irure enim.",
+    status: "in development",
+    technologies: "python",
+  },
+  {
+    title: "Hangman",
+    img: "",
+    description:
+      "Fugiat ut aute nostrud culpa fugiat sunt quis laboris incididunt veniam id commodo in ullamco. Sit officia culpa esse tempor non minim ea aliqua. Sit minim aliqua ex non Lorem aute dolore. Consectetur qui reprehenderit do aute enim est sunt labore occaecat adipisicing ea eiusmod. Reprehenderit laborum sint ullamco voluptate aliqua nisi in excepteur Lorem voluptate irure enim.",
+    status: "in development",
+    technologies: "javascropt, react, supabase",
   },
 ];
