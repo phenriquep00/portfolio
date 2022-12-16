89a7fbdf1e7e807b4dc5222cd8dc6e3f4ad5dc8d
Pedro Lima <contact.pedrolima@gmail.com>
2022-12-16 14:22:27 -0300

    updated project objectss

--- data.ts
+++ data.ts
@@ -1,9 +1,33 @@
+type status =
+  | "active"
+  | "offline"
+  | "in progress"
+  | "repository only"
+  | "completed";
+
+type technologies =
+  | "typescript"
+  | "python"
+  | "reactjs"
+  | "javascript"
+  | "nodejs"
+  | "react-native"
+  | "pygame"
+  | "pandas"
+  | "vite"
+  | "tailwindcss"
+  | "supabase"
+  | "html"
+  | "css"
+  | "bash"
+  | "...";
+
 export interface IData {
   title: string;
   img: string;
   description: string;
-  status: "active" | "offline" | "in development";
-  technologies: string;
+  status: status[];
+  technologies: technologies[];
 }
 
 export const data: IData[] = [
@@ -12,71 +36,104 @@ export const data: IData[] = [
     img: "",
     description:
       "Non tempor velit aute sunt labore eu deserunt eiusmod anim ex ad. Esse proident est esse pariatur irure. Do consectetur tempor officia aliqua fugiat officia excepteur labore adipisicing tempor est adipisicing nulla. Eiusmod eu est veniam incididunt fugiat labore amet elit. Labore duis dolore veniam veniam veniam exercitation dolor ullamco. Aliquip elit veniam ut ea aute do proident esse cillum. Amet reprehenderit laboris enim tempor labore quis esse magna cupidatat nulla nisi Lorem do in.",
-    status: "active",
-    technologies: "typescript, react, vite, tailwindcss, supabase, ...",
+    status: ["active", "in progress"],
+    technologies: [
+      "vite",
+      "typescript",
+      "reactjs",
+      "tailwindcss",
+      "supabase",
+      "html",
+      "css",
+    ],
   },
   {
     title: "Poker",
     img: "",
     description:
       "Fugiat ut aute nostrud culpa fugiat sunt quis laboris incididunt veniam id commodo in ullamco. Sit officia culpa esse tempor non minim ea aliqua. Sit minim aliqua ex non Lorem aute dolore. Consectetur qui reprehenderit do aute enim est sunt labore occaecat adipisicing ea eiusmod. Reprehenderit laborum sint ullamco voluptate aliqua nisi in excepteur Lorem voluptate irure enim.",
-    status: "in development",
-    technologies: "python, pygame",
+    status: ["active", "completed", "repository only"],
+    technologies: ["python", "pygame"],
   },
   {
     title: "Beat Machine",
     img: "",
     description:
       "Fugiat ut aute nostrud culpa fugiat sunt quis laboris incididunt veniam id commodo in ullamco. Sit officia culpa esse tempor non minim ea aliqua. Sit minim aliqua ex non Lorem aute dolore. Consectetur qui reprehenderit do aute enim est sunt labore occaecat adipisicing ea eiusmod. Reprehenderit laborum sint ullamco voluptate aliqua nisi in excepteur Lorem voluptate irure enim.",
-    status: "offline",
-    technologies: "python, pygame",
+    status: ["active", "completed", "repository only"],
+    technologies: ["python", "pygame"],
   },
   {
     title: "Nlw e-sports",
     img: "",
     description:
       "Fugiat ut aute nostrud culpa fugiat sunt quis laboris incididunt veniam id commodo in ullamco. Sit officia culpa esse tempor non minim ea aliqua. Sit minim aliqua ex non Lorem aute dolore. Consectetur qui reprehenderit do aute enim est sunt labore occaecat adipisicing ea eiusmod. Reprehenderit laborum sint ullamco voluptate aliqua nisi in excepteur Lorem voluptate irure enim.",
-    status: "in development",
-    technologies: "typescript, react, react-native, nodejs, tailwindcss",
+    status: ["completed", "offline", "repository only"],
+    technologies: [
+      "vite",
+      "typescript",
+      "nodejs",
+      "reactjs",
+      "react-native",
+      "html",
+      "tailwindcss",
+      "css",
+    ],
   },
   {
     title: "Page clones",
     img: "",
     description:
       "Fugiat ut aute nostrud culpa fugiat sunt quis laboris incididunt veniam id commodo in ullamco. Sit officia culpa esse tempor non minim ea aliqua. Sit minim aliqua ex non Lorem aute dolore. Consectetur qui reprehenderit do aute enim est sunt labore occaecat adipisicing ea eiusmod. Reprehenderit laborum sint ullamco voluptate aliqua nisi in excepteur Lorem voluptate irure enim.",
-    status: "in development",
-    technologies: "typescript, reacj, tailwindcss",
+    status: ["offline", "in progress"],
+    technologies: [
+      "vite",
+      "typescript",
+      "reactjs",
+      "tailwindcss",
+      "html",
+      "css",
+    ],
   },
   {
     title: "Imersão Dados",
     img: "",
     description:
       "Fugiat ut aute nostrud culpa fugiat sunt quis laboris incididunt veniam id commodo in ullamco. Sit officia culpa esse tempor non minim ea aliqua. Sit minim aliqua ex non Lorem aute dolore. Consectetur qui reprehenderit do aute enim est sunt labore occaecat adipisicing ea eiusmod. Reprehenderit laborum sint ullamco voluptate aliqua nisi in excepteur Lorem voluptate irure enim.",
-    status: "in development",
-    technologies: "Python, Pandas",
+    status: ["completed", "active", "repository only"],
+    technologies: ["python", "pandas"],
   },
   {
     title: "Nlw Return",
     img: "",
     description:
       "Fugiat ut aute nostrud culpa fugiat sunt quis laboris incididunt veniam id commodo in ullamco. Sit officia culpa esse tempor non minim ea aliqua. Sit minim aliqua ex non Lorem aute dolore. Consectetur qui reprehenderit do aute enim est sunt labore occaecat adipisicing ea eiusmod. Reprehenderit laborum sint ullamco voluptate aliqua nisi in excepteur Lorem voluptate irure enim.",
-    status: "in development",
-    technologies: "typescript, react, tailwindcss, nodejs, react-native",
+    status: ["completed", "repository only", "offline"],
+    technologies: [
+      "vite",
+      "react-native",
+      "reactjs",
+      "typescript",
+      "nodejs",
+      "tailwindcss",
+      "html",
+      "css",
+    ],
   },
   {
     title: "Pong",
     img: "",
     description:
       "Fugiat ut aute nostrud culpa fugiat sunt quis laboris incididunt veniam id commodo in ullamco. Sit officia culpa esse tempor non minim ea aliqua. Sit minim aliqua ex non Lorem aute dolore. Consectetur qui reprehenderit do aute enim est sunt labore occaecat adipisicing ea eiusmod. Reprehenderit laborum sint ullamco voluptate aliqua nisi in excepteur Lorem voluptate irure enim.",
-    status: "in development",
-    technologies: "python",
+    status: ["completed", "repository only"],
+    technologies: ["python"],
   },
   {
     title: "Hangman",
     img: "",
     description:
       "Fugiat ut aute nostrud culpa fugiat sunt quis laboris incididunt veniam id commodo in ullamco. Sit officia culpa esse tempor non minim ea aliqua. Sit minim aliqua ex non Lorem aute dolore. Consectetur qui reprehenderit do aute enim est sunt labore occaecat adipisicing ea eiusmod. Reprehenderit laborum sint ullamco voluptate aliqua nisi in excepteur Lorem voluptate irure enim.",
-    status: "in development",
-    technologies: "javascropt, react, supabase",
+    status: ["completed", "offline", "repository only"],
+    technologies: ["reactjs", "javascript", "css", "html", "supabase"],
   },
 ];
