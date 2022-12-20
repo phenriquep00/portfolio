d74a952014e6bdaeda4a0977bf65b725b3b37a2a
Pedro Lima <contact.pedrolima@gmail.com>
2022-12-20 14:10:48 -0300

    added all project descriptions

--- data.ts
+++ data.ts
@@ -50,21 +50,24 @@ export const data: IData[] = [
   {
     title: "Poker",
     img: "",
-    description: "",
+    description:
+      "A Poker game created with python & pygame. The players plays against 3 bots in a classic poker match where the winner is the last one with chips in the table.",
     status: ["active", "completed", "repository only"],
     technologies: ["python", "pygame"],
   },
   {
     title: "Beat Machine",
     img: "",
-    description: "",
+    description:
+      "A python & pygame application that simulates a drum kit. The app allows the user to select the beats per second and the amount of beats to repeat. The user can also save and import beats following a simple .txt file with the pattern.",
     status: ["active", "completed", "repository only"],
     technologies: ["python", "pygame"],
   },
   {
     title: "Nlw e-sports",
     img: "",
-    description: "",
+    description:
+      "A web projects developed during Rocketseat's Next Level Week Event. The project consists of a application to find people to play online games with you. The complete project is composed by a web app created with react, a mobile app built with react-native and a server created with nodejs.",
     status: ["completed", "offline", "repository only"],
     technologies: [
       "vite",
@@ -80,7 +83,7 @@ export const data: IData[] = [
   {
     title: "Page clones",
     img: "",
-    description: "",
+    description: "A react app that assembles my clones of web front-ends",
     status: ["offline", "in progress"],
     technologies: [
       "vite",
@@ -94,14 +97,15 @@ export const data: IData[] = [
   {
     title: "Imersão Dados",
     img: "",
-    description: "",
+    description: "A data science project created during Alura's Imersão Dados.",
     status: ["completed", "active", "repository only"],
     technologies: ["python", "pandas"],
   },
   {
     title: "Nlw Return",
     img: "",
-    description: "",
+    description:
+      "A project created during Rocketseat's Next Level Week. The goal of the event was to create a react component that allows the user to send feedback to the page maintainers. The feedback widget component is supported in web and mobile apps and is connected with a nodejs backend.",
     status: ["completed", "repository only", "offline"],
     technologies: [
       "vite",
@@ -117,14 +121,14 @@ export const data: IData[] = [
   {
     title: "Pong",
     img: "",
-    description: "",
+    description: "A simple pong game made with python's turtle library",
     status: ["completed", "repository only"],
     technologies: ["python"],
   },
   {
     title: "Hangman",
     img: "",
-    description: "",
+    description: "A hangman web application created with react and stylized with tailwindcss. The words come from a custom server on supabase",
     status: ["completed", "offline", "repository only"],
     technologies: ["reactjs", "javascript", "css", "html", "supabase"],
   },
