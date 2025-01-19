40b246c2d30c4f6a5b9a0ee67d5d4d9f0e466617
phenriquep00 <phenriquep00@gmail.com>
2025-01-19 16:42:56 -0300

    created structure for about me page

--- src/app/modules/about-me/about-me.component.ts
+++ src/app/modules/about-me/about-me.component.ts
@@ -1,4 +1,4 @@
-import { Component } from "@angular/core";
+import { Component, OnInit } from "@angular/core";
 import { TranslateService } from "@ngx-translate/core";
 
 @Component({
@@ -6,6 +6,15 @@ import { TranslateService } from "@ngx-translate/core";
     templateUrl: './about-me.component.html',
     standalone: false
 })
-export class AboutMeComponent {
+export class AboutMeComponent implements OnInit{
 
+    renderContent = false;
+    renderHologram = false;
+    renderProfile = false;
+
+    ngOnInit(): void {
+        setTimeout(() => {this.renderHologram = true}, 1500)
+        setTimeout(() => {this.renderContent = true}, 1500)
+        setTimeout(() => {this.renderProfile = true}, 2500)
+    }
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,63 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      animation: {
+        'zoom-in': 'zoomIn 1.5s ease-out forwards',
+        'slide-up': 'slideUp 1.2s ease-out forwards',
+        'slide-left': 'slideLeft 1.2s ease-out forwards',
+        'expand-horizontal': 'expandHorizontal 1.2s ease-out forwards',
+        'fly-vertical': 'flyVertical 3s linear forwards'
+      },
+      keyframes: {
+        zoomIn: {
+          '0%': {
+            transform: 'scale(0.1) translateY(-50%)',
+            opacity: 0,
+          },
+          '100%': {
+            transform: 'scale(1) translateY(0)',
+            opacity: 1,
+          },
+        },
+        slideUp: {
+          '0%': {
+            transform: 'translateY(100%)',
+            opacity: 0,
+          },
+          '100%': {
+            transform: 'translateY(0)',
+            opacity: 1,
+          },
+        },
+        slideLeft: {
+          '0%': {
+            transform: 'translateX(-100%)',
+            opacity: 0,
+          },
+          '100%': {
+            transform: 'translateX(0)',
+            opacity: 1,
+          },
+        },
+        expandHorizontal: {
+          '0%': {
+            transform: 'scaleX(0)',
+            opacity: 0,
+            transformOrigin: 'center',
+          },
+          '100%': {
+            transform: 'scaleX(1)',
+            opacity: 1,
+            transformOrigin: 'center',
+          },
+        },
+        flyVertical: {
+          '0%': { transform: 'translateY(100vh)' }, // Start off-screen at the bottom
+          '100%': { transform: 'translateY(-4vh)' } // End off-screen at the top
+        }
+      },
+    },
   },
   plugins: [],
 }
